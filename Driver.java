f6ebda0d519a850584f42259a6ec429613fbe366
anthonyqsun <qsun30@stuy.edu>
2022-01-08 15:56:00 -0500

    add test cases for different circumstances

--- Driver.java
+++ Driver.java
@@ -17,6 +17,33 @@ public class Driver{
     return retAL;
   }
 
+  public static ArrayList populateChoices(String choice, int size) {
+    ArrayList list = new ArrayList<Integer>(size);
+    if (choice.equals("ascending")) {
+      for (int i = 0; i < size; i++) {
+        list.add(i);
+      }
+    }
+    else if (choice.equals("descending")) {
+      for (int i = size-1; i >= 0; i--) {//descending
+        list.add(i);
+      }
+    }
+    else if (choice.equals("same")) {
+      for (int i = 0; i < size; i++) {
+        list.add(0);
+      }
+    }
+    else if (choice.equals("alternating")) {
+      int offset = 1;
+      for (int i = 0; i < size; i++) {
+        list.add(i+offset);
+        offset *= -1;
+      }
+    }
+    return list;
+  }
+
 //randomly rearrange elements of an ArrayList
   public static void shuffle( ArrayList al ) {
     int randomIndex;
@@ -36,16 +63,30 @@ public class Driver{
     for (int i = 1; i <= 5; i++){
       size += 10;
       System.out.println("\n\nTrial " + i + " using an ArrayList of Size " + size +":");
-      chrys = populate( size, 1, 1000 ); //array of size 10 with random integers from 1 to 1000
-      for (int n = 0; n < chrys.size(); n++){
+      chrys = populate( size, 1, 1000 ); 
+      for (int n = 0; n < chrys.size(); n++){ // copy chrys contents
         anthe.add(chrys.get(n));
         mum.add(chrys.get(n));
       }
-      System.out.println( "\nArrayList before sorting:\n" + chrys );
-      System.out.println("\nNumber of Swaps for Bubble Sort: " + Sorts.bubble(chrys));
-      System.out.println("\nNumber of Swaps for Selection Sort: " + Sorts.selection(anthe));
-      System.out.println("\nNumber of Swaps for Insertion Sort: " + Sorts.insertion(mum));
-      System.out.println( "\nArrayList after sorting:\n" + chrys );
+      System.out.println("ArrayList with random integers generated.");
+      System.out.println( "ArrayList before sorting:" + chrys );
+      System.out.println("Number of Swaps for Bubble Sort: " + Sorts.bubble(chrys));
+      System.out.println("Number of Swaps for Selection Sort: " + Sorts.selection(anthe));
+      System.out.println("Number of Swaps for Insertion Sort: " + Sorts.insertion(mum));
+      System.out.println( "ArrayList after sorting:" + chrys );
+
+      String[] choices = {"alternating", "descending", "same", "alternating"};
+      for (String choice : choices) {
+        ArrayList result = new ArrayList<Integer>(size);
+        result = populateChoices(choice, size);
+        System.out.println("\nArrayList with integers in " + choice + " order generated.");
+        System.out.println("Number of Swaps for Bubble Sort: " + Sorts.bubble(result));
+        System.out.println("Number of Swaps for Selection Sort: " + Sorts.selection(populateChoices(choice, size)));
+        System.out.println("Number of Swaps for Insertion Sort: " + Sorts.insertion(populateChoices(choice, size)));
+        System.out.println("ArrayList after sorting:" + result);
+      }
+
     }
+
   }
 }
